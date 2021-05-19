cade5723d4cee911518313c8585c13b26bafe776
ancoutard <antoine.coutard@etu.uca.fr>
2021-05-19 18:19:57 +0200

    ajout serviceWorker.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Project App</h2>
+      <p>Offline use ok.</p>
+      <p>Now we have to:</p>
+
+      <ul>
+        <li>Create the server.</li>
+        <li>Check PWA installation.</li>
+        <li>Develop the app.</li>
+      </ul>
     </div>
   );
 }
