21afe9327e23604010ea767fe174df16549b7772
ancoutard <antoine.coutard@etu.uca.fr>
2021-05-19 18:50:45 +0200

    ajout router + component Appartement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import AppartementDisplay from "./component/appartementDisplay/AppartementDisplay";
 
 function App() {
   return (
@@ -15,6 +17,12 @@ function App() {
         <li>Check PWA installation.</li>
         <li>Develop the app.</li>
       </ul>
+
+      <Switch>
+        <Route path="/appartement">
+          <AppartementDisplay />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ my-app/src/component/appartementDisplay/AppartementDisplay.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AppartementDisplay extends Component {
+    render() {
+      return <div>My first component</div>;
+    }
+}
+
+export default AppartementDisplay;
