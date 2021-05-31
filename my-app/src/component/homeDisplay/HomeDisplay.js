1143eca23500b1c5e35428a0faacb9357f8c7be4
Victor Janvier <VI.janvier@laposte.net>
2021-05-31 19:27:49 +0200

    aff des query

--- my-app/src/component/homeDisplay/HomeDisplay.js
+++ my-app/src/component/homeDisplay/HomeDisplay.js
@@ -1,17 +1,41 @@
-import React, { Component } from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { Component } from "react"
+import { useQuery } from "@apollo/react-hooks"
 import { GET_VILLE_ID } from "../../queries/villeQuery"
+
+let tab = []
 
 // Your first graphql hooks
 function CheckConfig() {
-  const { loading, error, data, networkStatus } = useQuery(GET_VILLE_ID, {variables: {id: '60a572d5f35096205030e5d5'}});
+  const { loading, error, data, networkStatus } = useQuery(GET_VILLE_ID, {
+    variables: { id: "60a572d5f35096205030e5d5" },
+  })
 
-  if (loading) return <span className="status-warning">LOADING</span>;
-  if (error) return <span className="status-error">{error.message}</span>;
-  if(data){
-    console.log(data)
+  if (loading) return <span className="status-warning">LOADING</span>
+  if (error) return <span className="status-error">{error.message}</span>
+  if (data) {
+    data.ville.immeubles.map((imm) => {
+      imm.appartements.map((app) => {
+        tab.push(
+          <>
+            <br />
+            <span>Nom : {data.ville.nom}</span>
+            <br />
+            <span>Code postal : {data.ville.codePostal}</span>
+            <br />
+            <span>Nom immeuble : {imm.nom}</span>
+            <br />
+            <span>Adresse immeuble: {imm.adresse}</span>
+            <br />
+            <span>Appartement numero : {app.numero}</span>
+            <br />
+            <span>Appartement nbPieces : {app.nbPieces}</span>
+          </>
+        )
+      })
+    })
+    return tab
   }
-  return <span className="status-ok">OK</span>;
+  return <span className="status-ok">OK</span>
 }
 
 class HomeDisplay extends Component {
@@ -23,8 +47,8 @@ class HomeDisplay extends Component {
           GraphQL status: <CheckConfig />
         </p>
       </>
-    );
+    )
   }
 }
 
-export default HomeDisplay;
+export default HomeDisplay
