148c22e1a79278f4508327b54630c884199f6536
Victor Janvier <VI.janvier@laposte.net>
2021-06-24 20:52:31 +0200

    Mise en place de la offline page

--- my-app/public/sw.js
+++ my-app/public/sw.js
@@ -1,47 +1,47 @@
 /* eslint-disable no-restricted-globals */
-console.log("Your custom ServiceWorker! 👍 ");
+console.log("Your custom ServiceWorker! 👍 ")
 
-var CACHE = "cache-update-and-refresh";
+var CACHE = "cache-update-and-refresh"
 
-self.addEventListener("install", function(evt) {
-  console.log("The service worker is being installed.");
+self.addEventListener("install", function (evt) {
+  console.log("The service worker is being installed.")
   evt.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      cache.addAll(["./index.html", "./assets"]);
+    caches.open(CACHE).then(function (cache) {
+      cache.addAll(["./index.html", "./assets"])
     })
-  );
-});
+  )
+})
 
-self.addEventListener("fetch", function(evt) {
-    console.log("The service worker is serving the asset.");
-    evt.respondWith(fromCache(evt.request));
-    evt.waitUntil(update(evt.request).then(refresh));
-  });
-  
-  function fromCache(request) {
-    return caches.open(CACHE).then(function(cache) {
-      return cache.match(request);
-    });
-  }
-  function update(request) {
-    return caches.open(CACHE).then(function(cache) {
-      return fetch(request).then(function(response) {
-        return cache.put(request, response.clone()).then(function() {
-          return response;
-        });
-      });
-    });
-  }
-  
-  function refresh(response) {
-    return self.clients.matchAll().then(function(clients) {
-      clients.forEach(function(client) {
-        var message = {
-          type: "refresh",
-          url: response.url,
-          eTag: response.headers.get("ETag")
-        };
-        client.postMessage(JSON.stringify(message));
-      });
-    });
-  }
+self.addEventListener("fetch", function (evt) {
+  console.log("The service worker is serving the asset.")
+  evt.respondWith(fromCache(evt.request))
+  evt.waitUntil(update(evt.request).then(refresh))
+})
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request)
+  })
+}
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response
+      })
+    })
+  })
+}
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: "refresh",
+        url: response.url,
+        eTag: response.headers.get("ETag"),
+      }
+      client.postMessage(JSON.stringify(message))
+    })
+  })
+}
